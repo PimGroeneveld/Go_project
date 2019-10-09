78eaa04acb57af38497e5e09223b5542ea72138a
Pim <pimgroeneveld92@gmail.com>
2019-10-09 22:00:28 +0200

    added some small if statement and array

--- hello.go
+++ hello.go
@@ -1,7 +1,26 @@
-package main
+package main // package to be able to exec file
 
 import "fmt"
 
 func main() {
-	fmt.Printf("hello, world\n")
+	var x int = 5 // "long" way
+	y := 10       // short hand
+
+	fmt.Println(x, y)
+	fmt.Printf("Hello, World!\n")
+
+	// basic if stuff
+	if x > y {
+		fmt.Printf("x is bigger than y")
+	} else if x < y {
+		fmt.Printf("nope x is smaller than y")
+	} else {
+		fmt.Printf("either equal or something has gone wrong")
+	}
+
+	//Basic array stuff
+	var array [5]int //index start at 0 like usual
+	array[3] = 2345678
+	fmt.Println(array)
+
 }
